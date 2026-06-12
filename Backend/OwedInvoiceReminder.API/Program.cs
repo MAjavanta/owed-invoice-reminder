de1fa9bcd9203254a309970944134459ef687398
MAjavanta <ma.javanta@gmail.com>
2026-06-12 14:27:07 +0100

    Create minimal API endpoint for getting invoices

--- Backend/OwedInvoiceReminder.API/Program.cs
+++ Backend/OwedInvoiceReminder.API/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,30 +13,75 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+var invoices = new List<Invoice>()
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new()
+    {
+        Id = 1,
+        ClientName = "Mansoor",
+        ClientEmail = "mahmad743@yahoo.co.uk",
+        InvoiceRef = "INV-01",
+        AmountPence = 2999,
+        DueDate = new DateOnly(2026,6,15),
+        Status = InvoiceStatus.Upcoming,
+        DateCreated = new DateOnly(2026,6,8)
+    },
+
+    new()
+    {
+        Id = 2,
+        ClientName = "Misha",
+        ClientEmail = "mahmad743@yahoo.co.uk",
+        InvoiceRef = "INV-02",
+        AmountPence = 9999,
+        DueDate = new DateOnly(2026,6,5),
+        Status = InvoiceStatus.Overdue,
+        DateCreated = new DateOnly(2026,6,1)
+    },
+    new()
+    {
+        Id = 3,
+        ClientName = "Evelyn",
+        ClientEmail = "evelyn.jean.chong@yahoo.co.uk",
+        InvoiceRef = "INV-03",
+        AmountPence = 999,
+        DueDate = new DateOnly(2026,05,15),
+        Status = InvoiceStatus.Paid,
+        DateCreated = new DateOnly(2026,05,08)
+    },
+    new()
+    {
+        Id = 4,
+        ClientName = "Dan",
+        ClientEmail = "mahmad743@yahoo.co.uk",
+        InvoiceRef = "INV-04",
+        AmountPence = 299,
+        DueDate = new DateOnly(2026,05,05),
+        Status = InvoiceStatus.Late,
+        DateCreated = new DateOnly(2026,04,08)
+    },
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/invoices", () => Results.Ok(invoices));
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class Invoice
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string ClientName { get; set; }
+    public string ClientEmail { get; set; }
+    public string InvoiceRef { get; set; }
+    public int AmountPence { get; set; }
+    public DateOnly DueDate { get; set; }
+    public DateOnly DateCreated { get; set; }
+    public InvoiceStatus Status { get; set; }
+}
+
+public enum InvoiceStatus
+{
+    Upcoming,
+    Overdue,
+    Paid,
+    Late
 }
